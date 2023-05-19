91d2e1cfb3003a80395b03dc83846304e409f01b
NikoMaersk <maerskp94@gmail.com>
2023-05-19 11:03:44 +0200

    MainPage
    restoring

--- src/main/java/View/MainPage.java
+++ src/main/java/View/MainPage.java
@@ -1,5 +1,25 @@
 package View;
 
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+
 public class MainPage extends Header
 {
+    public MainPage()
+    {
+        ScrollPane sp = new ScrollPane();
+        sp.setLayoutX(this.getxMargin());
+        sp.prefHeightProperty().bind(this.getScene().heightProperty().subtract(this.getLIMIT() + 50));
+        sp.prefWidthProperty().bind(this.getScene().widthProperty().subtract(this.getxMargin()*2));
+        //scrollPane.setStyle("-fx-background-color:transparent;");
+
+        GridPane gp = new GridPane();
+        gp.prefHeightProperty().bind(sp.prefHeightProperty());
+        gp.prefWidthProperty().bind(sp.prefWidthProperty());
+        gp.setGridLinesVisible(true); // debug
+
+        AnchorPane.setTopAnchor(sp, this.getLIMIT());
+        this.getAp().getChildren().add(sp);
+    }
 }
