c10ed7fd870b3b5732f290410245b15b02f96762
JakobPersson <jakper01@easv365.dk>
2023-05-23 10:16:05 +0200

    Added the necessary containers for the ProfilePage

---
+++ src/main/java/View/ProfilePage.java
@@ -0,0 +1,30 @@
+package View;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
+public class ProfilePage extends Header{
+
+    private ImageView profilePicture;
+
+    private TextField name;
+    private TextField address;
+    private TextField phoneNo;
+    private TextField email;
+    private TextField company;
+    private TextField bank;
+    private TextField accountNo;
+    private Button profileSave;
+
+
+    public ProfilePage()
+    {
+        setScene();
+    }
+
+    private void setScene() {
+    }
+
+
+}
