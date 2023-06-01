201ee245155655c3662e5dccf34293c2f5c695c3
rasmustofth <104558794+DARPZZ@users.noreply.github.com>
2023-06-01 08:34:50 +0200

    removed BookingsUd.java

--- src/main/java/com/example/park/HelloApplication.java
+++ src/main/java/com/example/park/HelloApplication.java
@@ -27,7 +27,6 @@ public class HelloApplication extends Application
     Login login = new Login();
     public static ProfilePage profilePage = new ProfilePage(); //Technicaly not nice, but profilePage is only refered to in a static context anyways
     Bookings bookings = new Bookings();
-    BookingsUd bookingsUd = new BookingsUd();
     Label toggleLabel = new Label("Press here to create user:");
     private final int HEIGHT = 768;
     private final int WIDTH = 1280;
@@ -44,7 +43,6 @@ public class HelloApplication extends Application
         login.subscribe(bookings); //tager
         login.subscribe(profilePage);
         login.setUserPublisher(login); // Giveren
-        login.subscribe(bookingsUd);
 
         primaryStageHolder = stage;
         primaryStageHolder.setMinWidth(400);
@@ -52,7 +50,6 @@ public class HelloApplication extends Application
         SCENE_MAP.put(SceneName.Bookings,bookings.SCENE);
         SCENE_MAP.put(SceneName.PlotPage, new PlotPage().SCENE);
         SCENE_MAP.put(SceneName.ProfilePage, profilePage.SCENE);
-       // SCENE_MAP.put(SceneName.BookingsUd,bookingsUd.SCENE);
         AnchorPane anchorPane = new AnchorPane();
 
         Scene scene = new Scene(anchorPane, WIDTH, HEIGHT);
