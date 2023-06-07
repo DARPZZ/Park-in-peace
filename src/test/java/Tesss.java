876df49ff870db722387305ce303152e63b09a4f
rasmustofth <104558794+DARPZZ@users.noreply.github.com>
2023-06-07 09:22:20 +0200

    Lavet junit test framework

---
+++ src/test/java/Tesss.java
@@ -0,0 +1,20 @@
+import Model.DaoObject.User;
+import Model.Implements.DaoUser;
+import com.example.park.Login;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class Tesss
+{
+
+    @BeforeEach
+    void setUp()
+    {
+    }
+    @AfterEach
+    void tearDown()
+    {
+    }
+}
