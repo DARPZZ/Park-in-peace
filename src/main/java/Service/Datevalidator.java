d44a04e65f8e7dd61b7cd131c22e42b148979948
rasmustofth <104558794+DARPZZ@users.noreply.github.com>
2023-06-08 10:04:10 +0200

    Lavet junit test date

--- src/main/java/Service/Datevalidator.java
+++ src/main/java/Service/Datevalidator.java
@@ -1,14 +1,22 @@
 package Service;
 
+import javafx.util.converter.LocalDateStringConverter;
+
+import java.sql.SQLOutput;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Datevalidator
 {
         private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-        public static boolean isValidDate(String date) {
+        public static boolean isValidDate(String date)
+        {
+            Calendar calendar = new GregorianCalendar(2024, Calendar.FEBRUARY, 1);
+            //System.out.println(calendar.getActualMaximum(Calendar.DAY_OF_MONTH));
             if (date == null || date.isEmpty()) {
                 return false;
             }
@@ -19,5 +27,8 @@ public class Datevalidator
             } catch (DateTimeParseException e) {
                 return false;
             }
+
         }
 }
+
+
--- src/test/com/example/park/HelloApplicationTest.java
+++ src/test/com/example/park/HelloApplicationTest.java
@@ -22,28 +22,37 @@ class HelloApplicationTest
     }
     @Test
     void testValidDate() {
+        System.out.println("Testing valid date ");
         assertTrue(dateValidator.isValidDate("2023-06-07"));
-        assertTrue(dateValidator.isValidDate("2022-12-31"));
+        System.out.println("Testing valid  month");
+        assertTrue(dateValidator.isValidDate("2022-10-09"));
     }
 
     @Test
     void testInvalidDate() {
+        System.out.println("Testing invalid date ");
         assertFalse(dateValidator.isValidDate("2023-06-32"));
+        System.out.println("Testing invalid Month ");
         assertFalse(dateValidator.isValidDate("2022-13-01"));
+        System.out.println("Testing invalid input ");
         assertFalse(dateValidator.isValidDate("abc"));
     }
 
     @Test
     void testBoundaryIn() {
-        assertTrue(dateValidator.isValidDate("2022-01-01"));
-        assertTrue(dateValidator.isValidDate("2023-06-07"));
+        System.out.println("Testing bounder in month");
+        assertTrue(dateValidator.isValidDate("2022-01-05"));
+        System.out.println("Testing boundery in Day");
+        assertTrue(dateValidator.isValidDate("2023-06-01"));
+        System.out.println("Testing bounder in month & day");
         assertTrue(dateValidator.isValidDate("2023-12-31"));
     }
 
     @Test
     void testBoundaryOut() {
-
+        System.out.println("Testing boundery out invalid day");
         assertFalse(dateValidator.isValidDate("2021-12-32"));
-        assertFalse(dateValidator.isValidDate("2024-05-32"));
+        System.out.println("Testing boundery out invalid month");
+        assertFalse(dateValidator.isValidDate("2024-13-22"));
     }
 }
